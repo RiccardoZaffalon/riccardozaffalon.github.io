6d342df3429db3172bc2cb9566395e62d3563ae4
Riccardo Zaffalon <rzaffalon@alpenite.com>
2021-12-21 17:07:48 +0100

    Remove boilerplate, add base content

---
+++ pages/index.jsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <main className='max-w-4xl py-16 mx-auto'>
+      <div className="px-4">
+        <h1 className="text-6xl font-bold mb-8">Riccardo Zaffalon</h1>
+        <h2 className="text-2xl md:text-4xl mb-8">A frontend developer working in Venice, Italy.</h2>
+        <p className="text-lg md:text-3xl mb-8">I specialize in building fast and enjoyable web experiences, using modern technologies and user-centered design.</p>
+        <p><a href="#contacts">Get in touch →</a></p>
+      </div>
+    </main>
+  )
+}
