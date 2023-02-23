e1159564959c78902325289033c3d59707075951
Riccardo Zaffalon <riccardo.zaffalon@gmail.com>
2023-02-23 10:11:10 +0100

    Replace Parcel with Vite, Sass with Tailwind, restyle

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/index.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+};
